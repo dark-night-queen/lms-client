881231f3c329efbf65f9349e791805ba1991a70c
Shreya <agrawal.shreya@icloud.com>
2025-10-21 14:30:08 +0530

    added team page components

--- src/app/(main)/team/page.tsx
+++ src/app/(main)/team/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { DataTable } from "./_components/data-table";
+
+import data from "./data.json";
 
 const Team = () => {
-  return <div>Team</div>;
+  return (
+    <div className="p-4">
+      <DataTable data={data} />
+    </div>
+  );
 };
 
 export default Team;
